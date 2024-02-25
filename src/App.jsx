88aca404294b4f8d05143b4aa93fda6edb3e586b
sakt-hi <sakthivel.ganesan@hotmail.com>
2024-02-25 23:14:03 +0530

    router-change

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
+// App.jsx
 import React from 'react';
-import { Outlet } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import { DataProvider } from './utils/DataContext';
 import Footer from './components/Footer';
 import DevInfo from './components/DevInfo';
+import Home from './components/Home';
+import About from './components/About';
 
-const AppLayout = () => {
+const App = () => {
+
     return (
         <DataProvider>
             <div className="app">
-                <Header />
-                <Outlet className="outlet" />
-                <DevInfo />
-                <Footer />
+                <Router>
+                    <Header />
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                    <DevInfo />
+                    <Footer />
+                </Router>
             </div>
         </DataProvider>
     );
 }
 
-export default AppLayout;
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,13 @@
+// main.jsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from './App';
-import './index.scss'
-import Home from './components/Home'
-import About from './components/About'
-
-const appRouter = createBrowserRouter([
-    {
-        path: "/",
-        element: <App />,
-        children: [
-            {
-                path: "/",
-                element: <Home />,
-            },
-            {
-                path: "/about",
-                element: <About />,
-            },
-        ],
-        errorElement: <div>Error</div>,
-    }
-]);
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-    <RouterProvider router={appRouter}>
-        <Router>
-            <App />
-        </Router>
-    </RouterProvider>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
